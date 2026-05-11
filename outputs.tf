81db048c76ba9e87bafa709a79c90c62768defd6
Nkwele Ngole <Nkwelengole@gmail.com>
2026-05-11 01:10:37 -0500

    outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,14 @@
+# Display the resource group name after deployment
 output "resource_group_name" {
   value = azurerm_resource_group.lab.name
 }
 
+# Display the virtual network address space after deployment
 output "vnet_address_space" {
   value = azurerm_virtual_network.lab_vnet.address_space
 }
 
+# Display the NSG name after deployment
 output "nsg_name" {
   value = azurerm_network_security_group.lab_nsg.name
 }
