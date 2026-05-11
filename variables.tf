422511d3824278fa5b09a7c1f0e05ef89d12b4f2
Nkwele Ngole <Nkwelengole@gmail.com>
2026-05-11 01:10:19 -0500

    variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,12 @@
+# Name of the Azure resource group that will contain all lab resources
 variable "resource_group_name" {
   description = "Name of the resource group"
   default     = "terraform-security-lab"
 }
 
+# Azure region where all resources will be deployed
+# Change this to deploy to a different region
 variable "location" {
   description = "Azure region"
-  default     = "West US"
+  default     = "East US"
 }
